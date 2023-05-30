e232e591533b91d81e5d11167a932d47103e4671
avtsoof <tsoof10@gmail.com>
2023-05-30 20:24:51 +0300

    update

--- app.py
+++ app.py
@@ -6,38 +6,49 @@ DATA_URL = (
     "Motor_Vehicle_Collisions_-_Crashes.csv"
 )
 
-n_injured_col = "number of persons injured"
+N_INJURED_COL = "NUMBER OF PERSONS INJURED"
 
 def main():
     st.title("Motor Vehicle Collisions")
     
     df = load_data(100000)
-    n_max_injured = int(df[n_injured_col].max())
-    n_min_injured = int(df[n_injured_col].min())
-            
-    st.header("Where are the most people injured in NYC?")
-    injured_people = st.slider("No. persons injured", min_value=n_min_injured, max_value=n_max_injured)
-    injured_people_df = df.query("`number of persons injured` >= @injured_people")
-    injured_people_lonlat_df = injured_people_df[["latitude", "longitude"]]
-    injured_people_lonlat_df = injured_people_lonlat_df.dropna(how="any")
-    st.map(injured_people_lonlat_df)
-    
     widget_show_raw_data(df)
-
-
+    widget_show_2d_map(df)
+    widget_show_3d_map(df)
+    
+    
 def widget_show_raw_data(df):
-    if st.checkbox("Show Raw Data", False):
+    if st.sidebar.checkbox("Show Raw Data", False):
         st.subheader("Raw Data")
         st.write(df)
 
 
+def widget_show_2d_map(df):
+    n_max_injured = int(df[N_INJURED_COL].max())
+    n_min_injured = int(df[N_INJURED_COL].min())
+    
+    injured_people = st.sidebar.slider("No. persons injured", min_value=n_min_injured, max_value=n_max_injured)
+    
+    df_subset = df.query(F"`{N_INJURED_COL}` >= @injured_people")
+    
+    if st.sidebar.checkbox("Specify Hour"):
+        hour = st.sidebar.slider("Hour to look at", 0, 23, value=8, step=1)
+        df_subset = df_subset[df_subset["CRASH DATE_CRASH TIME"].dt.hour == hour]
+    
+    st.header("Where are the most people injured in NYC?")
+    st.map(df_subset[["LATITUDE", "LONGITUDE"]])
+    st.write(f"TOTAL:", len(df_subset))
+    st.write(df_subset)
+
+
+def widget_show_3d_map(df):
+    pass
+    
+
 @st.cache_resource()
 def load_data(nrows: int = None):
-    df = pd.read_csv(DATA_URL, nrows=nrows)
-    df.dropna(subset=["LATITUDE", "LONGITUDE"], inplace=True)
-    lowercase = lambda colname: str(colname).lower()
-    df.rename(lowercase, axis='columns', inplace=True)
-    df.rename(columns={"crash_date_crash_time": "date_time"}, inplace=True)
+    df = pd.read_csv(DATA_URL, nrows=nrows, parse_dates=[["CRASH DATE", "CRASH TIME"]])
+    df.dropna(subset=["LATITUDE", "LONGITUDE"], inplace=True, how="any")
     return df
     
 
